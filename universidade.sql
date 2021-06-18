9080a0caa2c4c4bebe0e20428eb72317951193aa
Matheus Reis <matheusjosereis99@gmail.com>
2021-06-18 11:02:22 -0300

    Fixing interpretation problems

--- universidade.sql
+++ universidade.sql
@@ -8,7 +8,12 @@ CREATE TABLE faculdade(
   numeroProfessores int,
   numeroAlunos int, 
   orcamento float,
+  idDiretor int,
 
+  -- Disciplinas oferecidas 
+  idDisciplina int,
+
+  foreign key (idDiretor) REFERENCES professor(idProfessor),
   primary key (siglaFaculdade),
   unique (nomeBloco)
 );
@@ -20,7 +25,9 @@ CREATE TABLE aluno(
   idAluno int,
   dataNascimento date,
   nomeAluno varchar(50),
-  
+  siglaFaculdade varchar(8),
+
+  foreign key (siglaFaculdade) REFERENCES faculdade(siglaFaculdade),  
   primary key (idAluno),
   unique (nomeAluno)
 );
@@ -31,7 +38,11 @@ CREATE TABLE professor(
   nomeProfessor varchar(50),
   dataNascimento date,
   salario float,
+  siglaFaculdade varchar(8),
+  idTurma int,
 
+  foreign key (idTurma) REFERENCES turma(idTurma),
+  foreign key (siglaFaculdade) REFERENCES faculdade(siglaFaculdade),
   primary key (idProfessor)
 );
 
@@ -40,9 +51,11 @@ CREATE TABLE disciplina(
   nomeDisciplina varchar(50),
   siglaDisciplina varchar(8),
   numeroCreditos int,
-  idResponsavel int,
+  idResponsavel int,  
   siglaSuperDisciplina varchar(8),  --Pré-requisito
-
+  siglaFaculdade varchar(8),
+  
+  foreign key (siglaFaculdade) REFERENCES faculdade(siglaFaculdade),
   foreign key (siglaSuperDisciplina) REFERENCES disciplina(siglaDisciplina),
   primary key (siglaDisciplina),
   foreign key (idResponsavel) REFERENCES professor(idProfessor)
@@ -56,13 +69,15 @@ CREATE TABLE turma(
   idTurma int, 
   lugar varchar(20),
 
+  foreign key (lugar) REFERENCES faculdade(nomeBloco),
   primary key (idTurma)
 );
+
 
 CREATE TABLE sala(
   nomeBloco varchar(3),
   numeroSala int,
   capacidadeSala int,
-
+  
   foreign key (nomeBloco) REFERENCES faculdade(nomeBloco)
 );
